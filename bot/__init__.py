86ee122b7bc4a1a38ad158d3b4b671bede475f95
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-06-17 07:20:47 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -24,7 +24,7 @@ class Config(object):
     BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
     DATABASE_URL = str(os.environ.get("DATABASE_URL"))
     USERNAME = str(os.environ.get("BOT_USERNAME"))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+    LOG_CHANNEL = int(os.environ.get("-1002173308278"))
     AUTH_USERS = list(
         set(
             int(x)
